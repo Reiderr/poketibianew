cbe6f3ee1605cb9f76f571dab83a0d62850ccdcb
Reiderr <marcosreider@hotmail.com>
2017-03-30 00:30:44 -0300

    adicionado hearthstone cidade

---
+++ PokeAlpha Server/data/actions/scripts/templechange.lua
@@ -0,0 +1,60 @@
+function onUse(cid, item, frompos, item2, topos)
+
+    estatua1 = 3837 ---uniqueid da estatua na city 1
+    estatua2 = 3838 ---uniqueid da estatua na city 2
+    estatua3 = 3839 ---uniqueid da estatua na city 3
+    estatua4 = 3840
+    estatua5 = 3841
+    estatua6 = 3842
+    estatua7 = 3843
+    estatua8 = 3844
+    estatua9 = 3845
+    estatua10 = 3846
+
+    estatua = 3697 ---actionid das estatuas 
+
+
+    if item.uid == estatua1 then
+        doPlayerSetTown(cid, 10)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Pallet.")
+
+    elseif  item.uid == estatua2 then
+        doPlayerSetTown(cid, 1)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Saffron.")
+
+    elseif  item.uid == estatua3 then
+        doPlayerSetTown(cid, 2)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Cerulean.")
+
+    elseif  item.uid == estatua4 then
+        doPlayerSetTown(cid, 3)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Celladon.")
+
+    elseif  item.uid == estatua5 then
+        doPlayerSetTown(cid, 4)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Lavender.")
+
+    elseif  item.uid == estatua6 then
+        doPlayerSetTown(cid, 5)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Vermilion.")
+
+    elseif  item.uid == estatua7 then
+        doPlayerSetTown(cid, 6)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Fuchsia.")
+
+    elseif  item.uid == estatua8 then
+        doPlayerSetTown(cid, 7)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Cinnabar.")
+
+    elseif  item.uid == estatua9 then
+        doPlayerSetTown(cid, 8)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Viridian.")
+
+    elseif  item.uid == estatua10 then
+        doPlayerSetTown(cid, 9)
+        doPlayerSendTextMessage(cid,22,"Você agora é um cidadão de Pewter.")
+
+
+    end
+    return 1
+end
